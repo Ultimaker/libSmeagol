f10409a8ea6b9baa2f5507e2431fc957c63bea39
Onno Broekmans <o.broekmans@ultimaker.com>
2022-04-29 17:35:18 +0200

    Clean up Pocket tests
    Simplify and clean up the testing code for the `Pocket` class:
    
    - Rewrite `get/setAs{Boolean,String}` tests to use parametrization. The
      `test_getAs*` and `test_setAs*` testcases for are easier to maintain
      and interpret if we make use of pytest's parametrized testing
      facility.
    
    - Drop the rigid "arrange, act, assert" structure, and go for a more
      readable, less verbose flow focused on use cases.
    
    - Drop the rigid use of local variables to store, e.g., key names. Tests
      are more readable if the number of indirections is a bit lower.

--- tests/testPocket.py
+++ tests/testPocket.py
@@ -5,32 +5,6 @@ import pytest
 from libSmeagol import Pocket
 
 
-def _update_pocket(pocket: Pocket, settings: dict) -> None:
-    """
-    Add settings to the pocket
-
-    :param pocket: Pocket to update.
-    :param settings: A dictionary of settings
-    """
-    #  @param
-    for key, value in settings.items():
-        pocket.set(key, value)
-
-
-def test_unknown_key():
-    """Test unknown key with default value"""
-    # Arrange
-    key = "KeyDoesNotExist"
-    default_value = "the_default_value"
-    pocket = Pocket()
-
-    # Act
-    value = pocket.get(key, default_value)
-
-    # Assert
-    assert value == default_value
-
-
 def test_init_with_pocket():
     """We can initialize a Pocket with another Pocket, in which case it should create an
     independent deepcopy of the original"""
@@ -45,253 +19,139 @@ def test_init_with_pocket():
     assert orig_pocket.get("string") == "test_case"
 
 
-def test_getAsString():
+def test_unknown_key():
+    """Test unknown key with default value"""
+    pocket = Pocket()
+
+    value = pocket.get("UnknownKey", "DefaultValue")
+    assert value == "DefaultValue"
+
+
+@pytest.mark.parametrize("name,value,expected", [
+    ("int", 101, "101"),
+    ("float", 13.501, "13.501"),
+    ("bool", True, "True"),
+    ("string", "test_case", "test_case"),
+])
+def test_getSetAsString(name, value, expected):
     """Test getAsString conversions"""
-    # Arrange
-    settings = {"int": 101, "float": 13.501, "bool": True, "string": "test_case"}
-    expected = {"int": "101", "float": "13.501", "bool": "True", "string": "test_case"}
+
+    # getAsString forces the set value to be read out as a string:
     pocket = Pocket()
+    pocket.set(name, value)
+    assert pocket.getAsString(name) == expected
 
-    # Act
-    _update_pocket(pocket, settings)
-
-    # Assert
-    for key, value in expected.items():
-        actual_value = pocket.getAsString(key)
-        assert value == actual_value
-
-
-def test_setAsString():
-    """Test setAsString conversions"""
-    # Arrange
-    settings = {"int": 101, "float": 13.501, "bool": True, "string": "test_case"}
-    expected = {"int": "101", "float": "13.501", "bool": "True", "string": "test_case"}
+    # Conversely, setAsString forces the value to be stored as a string:
     pocket = Pocket()
-
-    # Act
-    for key, value in settings.items():
-        pocket.setAsString(key, value)
-
-    # Assert
-    for key, value in expected.items():
-        actual_value = pocket.get(key)
-        assert value == actual_value
+    pocket.setAsString(name, value)
+    assert pocket.get(name) == expected
 
 
-def test_getAsBoolean():
+@pytest.mark.parametrize("name,value,expected", [
+    ("int_zero", 0, False),
+    ("int_not_zero", 1, True),
+    ("int_negative", -1, True),
+    ("float", 13.501, True),
+    ("bool_true", True, True),
+    ("bool_false", False, False),
+    ("string_yes", "yes", True),
+    ("string_true", "true", True),
+    ("string_no", "no", False),
+    ("string_false", "false", False),
+    ("string_1", "1", True),
+    ("string_0", "0", False),
+    ("string_float_0", "0.00", False),
+    ("string_float_1", "13.5e-3", True),
+    ("string_float_not_zero", "13.37", True),
+])
+def test_setGetAsBoolean(name, value, expected):
     """Test getAsBoolean conversions"""
-    # Arrange
-    settings = {
-        "int_zero": 0,
-        "int_not_zero": 1,
-        "int_positive": 1,
-        "float": 13.501,
-        "bool_true": True,
-        "bool_false": False,
-        "string_yes": "yes",
-        "string_true": "true",
-        "string_no": "no",
-        "string_false": "false",
-        "string_1": "1",
-        "string_0": "0",
-        "float_0": "0.00",
-        "float_not_zero": "13.37",
-    }
-    expected = {
-        "int_zero": False,
-        "int_not_zero": True,
-        "int_positive": True,
-        "float": True,
-        "bool_true": True,
-        "bool_false": False,
-        "string_yes": True,
-        "string_true": True,
-        "string_no": False,
-        "string_false": False,
-        "string_1": True,
-        "string_0": False,
-        "float_0": False,
-        "float_not_zero": True,
-    }
+
+    # getAsBoolean forces the set  value to be read out as a boolean:
     pocket = Pocket()
+    pocket.set(name, value)
+    assert pocket.getAsBoolean(name) == expected
 
-    # Act
-    _update_pocket(pocket, settings)
-
-    # Assert
-    for key, value in expected.items():
-        actual_value = pocket.getAsBoolean(key)
-        assert value == actual_value
-
-
-def test_setAsBoolean():
-    """Test setAsBoolean conversions"""
-    # Arrange
-    settings = {"int_zero": 0, "int_not_zero": 1, "int_positive": 1, "float": 13.501, "bool_true": True, "bool_false": False, "string_yes": "yes", "string_true": "true", "string_no": "no", "string_false": "false", "string_1": "1", "string_0": "0", "float_0": "0.00", "float_not_zero": "13.37"}
-    expected = {"int_zero": False, "int_not_zero": True, "int_positive": True, "float": True, "bool_true": True, "bool_false": False, "string_yes": True, "string_true": True, "string_no": False, "string_false": False, "string_1": True, "string_0": False, "float_0": False, "float_not_zero": True}
+    # Conversely, setAsBoolean forces the value to be stored as a boolean:
     pocket = Pocket()
-
-    # Act
-    for key, value in settings.items():
-        pocket.setAsBoolean(key, value)
-
-    # Assert
-    for key, value in expected.items():
-        actual_value = pocket.get(key)
-        assert value == actual_value
+    pocket.setAsBoolean(name, value)
+    assert pocket.get(name) == expected
 
 
-def test_unable_conversions():
-    """Test some unable conversions (from string to int and vv)"""
-    # Arrange
-    settings = {"not_a_number": "nan", "not_numeric": "abc"}
-    expected = {"not_a_number": None, "not_numeric": None}
+@pytest.mark.parametrize("string_value", ["nan", "abc"])
+def test_invalid_conversions(string_value):
+    """
+    Test some invalid type conversions: these should always fall back
+    to default values
+    """
     pocket = Pocket()
+    pocket.setAsString("value", string_value)
 
-    # Act
-    for key, value in settings.items():
-        pocket.setAsString(key, value)
-
-    # Assert
-    for key, value in expected.items():
-        actual_value = pocket.getAsInt(key)
-        assert value == actual_value
+    assert pocket.getAsInt("value") is None
 
 
-def test_setAsPocket():
+def test_getSetAsSubPocket():
     """Test setting a dictionary value as pocket"""
-    # Arrange
     settings = {"int": 101, "float": 13.501, "bool": True, "string": "test_case"}
-    key = "test_reg"
+
     pocket = Pocket()
+    subpocket = Pocket(settings)
+    pocket.setAsSubPocket("subpocket", subpocket)
 
-    # Act
-    sub_pocket = Pocket(settings)
-    pocket.setAsSubPocket(key, sub_pocket)
+    # After storing the subpocket, we can retrieve it
+    assert pocket.get("subpocket") == settings
+    assert pocket.getAsSubPocket("subpocket").getAll() == settings
 
-    # Assert
-    expected = pocket.get(key)
-    assert expected == settings
-
-
-def test_getAsPocket():
-    """Test getting a pocket value"""
-    # Arrange
-    settings = {"int": 101, "float": 13.501, "bool": True, "string": "test_case"}
-    key = "test_reg"
-    pocket = Pocket()
-
-    # Act
-    pocket.set(key, settings)
-
-    # Assert
-    sub_pocket = pocket.getAsSubPocket(key)
-    assert sub_pocket.getAll() == settings
-
-
-def test_set_boolean_as_pocket():
-    """Test setting a pocket using a boolean value"""
-    pocket = Pocket()
-
+    # We cannot call setAsSubPocket with anything but a Pocket
     with pytest.raises(ValueError):
-        pocket.setAsSubPocket(key="test_set_boolean_pocket", value=True)
+        pocket.setAsSubPocket("no_pocket", 123)
+
+    # Calling getAsSubPocket on a non-dict value gives us the default value
+    assert subpocket.getAsSubPocket("int") is None
 
 
-def test_getAsSubPocket_from_boolean():
-    """Test getting a pocket using from a boolean value"""
-    # Arrange
-    setting = True
-    key = "test_get_boolean_pocket"
-    expected = None
-    pocket = Pocket()
-
-    # Act
-    pocket.set(key, setting)
-
-    # Assert
-    value = pocket.getAsSubPocket(key)
-    assert expected == value
-
-
-def test_geeting_pocket_from_unknown_key():
+def test_getAsSubPocket_unknown_key():
     """Test getting a pocket using from an unknown key"""
-    # Arrange
-    key = "test_bogus_pocket"
-    expected = {}
     pocket = Pocket()
 
-    # Act
-    value = pocket.getAsSubPocket(key)
+    # Retrieving an known subpocket gives us an empty Pocket.
+    subpocket = pocket.getAsSubPocket("unknown")
+    assert isinstance(subpocket, Pocket)
+    assert subpocket.getAll() == {}
 
-    # Assert
-    assert isinstance(value, Pocket)
-    assert expected == value.getAll()
+    # After retrieving the unknown sub-Pocket, it has magically materialized
+    # in the parent Pocket. (This is bizarre behavior, but it's what's currently
+    # implemented.)
+    assert pocket.has("unknown")
+    assert pocket.get("unknown") == {}
 
 
-def test_setAsList():
+def test_getSetAsList():
     """Test setting a list"""
-    # Arrange
-    settings = ["aap", "noot", "mies"]
-    key = "list_set_key"
+    settings = ["alpha", "bravo", "charlie"]
+
+    # setAsList stores a copy of the list
     pocket = Pocket()
+    pocket.setAsList("a_list", settings)
 
-    # Act
-    pocket.setAsList(key, settings)
+    a_list = pocket.get("a_list")
+    assert isinstance(a_list, list)
+    assert a_list == settings
 
-    # Assert
-    value = pocket.get(key)
-    assert isinstance(value, list)
-    assert settings == value
+    # The returned list is a copy:
+    a_list.append("delta")
+    assert len(pocket.get("a_list")) == 3
 
-
-def test_getAsList():
-    """Test getting a list"""
-    # Arrange
-    settings = [ "aap", "noot", "mies" ]
-    key = "list_get_key"
-    pocket = Pocket()
-
-    # Act
-    pocket.set(key, settings)
-
-    # Assert
-    value = pocket.getAsList(key)
-    assert isinstance(value, list)
-    assert settings == value
-
-
-def test_set_boolean_as_list():
-    """Test setting a list using a boolean value"""
-    pocket = Pocket()
-
+    # setAsList only accepts lists:
     with pytest.raises(ValueError):
-        pocket.setAsList(key="test_set_boolean_list", value=True)
+        pocket.setAsList("not_a_list", 123)
 
+    # Calling getAsList on a non-list value gives us the default value, None
+    pocket.set("not_a_list", 123)
+    assert pocket.getAsList("not_a_list") is None
 
-def test_getAsList_from_boolean():
-    """Test get list from a boolean"""
-    setting = True
-    key = "test_get_boolean_list"
-
-    pocket = Pocket()
-    pocket.set(key, setting)
-
-    value = pocket.getAsList(key)
-    assert value is None
-
-
-def test_getting_list_from_unknown_key():
-    """Test getting a list using from an unknown key"""
-    # Arrange
-    key = "test_bogus_list"
-    expected = None
-    pocket = Pocket()
-
-    # Act
-    value = pocket.getAsList(key)
-
-    # Assert
-    assert expected == value
+    # Calling getAsList on an unknown key also gives us None
+    assert pocket.getAsList("unknown_key") is None
 
 
 def test_nested_pocket():
@@ -301,38 +161,47 @@ def test_nested_pocket():
         "nozzle": {
             "1": {
                 "material_id": "6976d020-18d1-4d46-9f3e-411189a1e230",
-                "skew": [ 0.0, -0.02],
+                "skew": [0.0, -0.02],
                 "size": 0.4
-           },
+            },
             "2": {
                 "material_id": "6976d020-18d1-4d46-9f3e-411189a1e230",
-                "skew": [ 0.1, -0.01],
+                "skew": [0.1, -0.01],
                 "size": 0.25
-           }
-       }
-   }
-    key = "head"
+            }
+        }
+    }
     pocket = Pocket()
-    pocket.set(key, settings)
+    pocket.set("head", settings)
 
-    # Act
-    head = pocket.getAsSubPocket(key)
+    # Get nested dicts as sub-Pocket objects:
+    head = pocket.getAsSubPocket("head")
     assert head is not None
-    nozzle = head.getAsSubPocket("nozzle")
 
+    nozzle = head.getAsSubPocket("nozzle")
+    assert nozzle is not None
+
+    # We can add values to the nested dicts:
     nozzle_1 = nozzle.getAsSubPocket("1", None)
-    nozzle_2 = nozzle.getAsSubPocket("2", None)
-    nozzle_3 = nozzle.getAsSubPocket("3")
+    assert nozzle_1 is not None
 
     nozzle_1.setAsInt("max_temperature", 120)
+
+    assert "max_temperature" in pocket.get("head")["nozzle"]["1"]
+    assert pocket.get("head")["nozzle"]["1"]["max_temperature"] == 120
+
+    # We can modify values in the nested dicts:
+    nozzle_2 = nozzle.getAsSubPocket("2", None)
+    assert nozzle_2 is not None
+
     nozzle_2.setAsFloat("size", 0.4)
+
+    assert pocket.get("head")["nozzle"]["2"]["size"] == 0.4
+
+    # We can automagically create new subpockets by requesting acccess to them:
+    nozzle_3 = nozzle.getAsSubPocket("3")
+    assert nozzle_3 is not None
+
     nozzle_3.setAsFloat("new_data", 0.4)
 
-    # Assert
-    assert nozzle_1 is not None
-    assert nozzle_2 is not None
-    updated_settings = pocket.get(key)
-    assert "max_temperature" in updated_settings["nozzle"]["1"]
-    assert 120 == updated_settings["nozzle"]["1"]["max_temperature"]
-    assert 0.4 == updated_settings["nozzle"]["2"]["size"]
-    assert 0.4 == updated_settings["nozzle"]["3"]["new_data"]
+    assert pocket.get("head")["nozzle"]["3"]["new_data"] == 0.4
