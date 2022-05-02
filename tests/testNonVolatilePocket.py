8e4bd73914704f1743589262ad65aad2b195df34
Onno Broekmans <o.broekmans@ultimaker.com>
2022-05-02 18:34:58 +0200

    Add tests for NonVolatilePocket class
    Not all methods in the `NonVolatilePocket` public API were properly
    tested; add tests for `erase` and `backupAndSetup`.

--- tests/testNonVolatilePocket.py
+++ tests/testNonVolatilePocket.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, Optional
 
 import pytest
 
-from libSmeagol import NonVolatilePocket
+from libSmeagol import Pocket, NonVolatilePocket
 
 
 DEFAULT_SAVE_INTERVAL = 2
@@ -32,21 +32,6 @@ def pocket(make_pocket) -> NonVolatilePocket:
     return make_pocket()
 
 
-def test_save_preference(pocket: NonVolatilePocket):
-    """Simple set value / wait for save / verify save test (implies get/set test)"""
-    # Arrange
-    key = "TestKey"
-    value = "dummy_value"
-
-    # Act
-    pocket.setAsString(key, value)
-    pocket.forceSave()
-    settings = _load_settings_from_pocket_file(pocket)
-
-    # Assert
-    assert value == settings[key]
-
-
 def _load_settings_from_pocket_file(pocket: NonVolatilePocket) -> Any:
     """Load the JSON file created by the Pocket and return the contents as a dictionary"""
     print("Loading file " + pocket.getFilename())
@@ -54,21 +39,91 @@ def _load_settings_from_pocket_file(pocket: NonVolatilePocket) -> Any:
         return json.load(f)
 
 
-def test_erase(make_pocket):
-    pocket = make_pocket()
-    print(f'@@ {pocket.getFilename()}')
+def test_save_load(pocket: NonVolatilePocket):
+    """Test basic storage of Pocket data"""
+
+    # Set up some basic keys, plus a nested SubPocket.
+    pocket.setAsInt("my_int", 42)
+    pocket.setAsString("foo", "bar")
+    pocket.setAsSubPocket("sub_pocket", Pocket({"my_float": 3.14, "baz": "bam"}))
+
+    # The underlying data can be retrieved from the JSON file.
+    pocket.forceSave()
+    settings = _load_settings_from_pocket_file(pocket)
+    assert settings == {
+        "my_int": 42,
+        "foo": "bar",
+        "sub_pocket": {
+            "my_float": 3.14,
+            "baz": "bam",
+        }
+    }
+
+    # We can also use a new Pocket object to retrieve the data.
+    new_pocket = NonVolatilePocket(pocket.getFilename())
+    assert new_pocket.get("my_int") == 42
+    assert new_pocket.get("foo") == "bar"
+
+    sub_pocket = new_pocket.getAsSubPocket("sub_pocket")
+    assert isinstance(sub_pocket, Pocket)
+    assert sub_pocket.getAll() == {"my_float": 3.14, "baz": "bam"}
+
+
+def test_erase(pocket):
+    """A NonVolatilePocket can be reset to an empty dict"""
+
     pocket.setAsInt("my_int", 42)
     pocket.setAsString("my_string", "FooBar")
+    pocket.setAsSubPocket("sub_pocket", Pocket({"my_float": 3.14, "foo": "bar"}))
     pocket.forceSave()
+
+    assert pocket.has("my_int")
+    assert pocket.has("my_string")
+    assert pocket.has("sub_pocket")
 
     # MISP-1812: In the original implementation of TimerPocket,
     # NonVolatilePocket.erase() would fail with a RunTimeError (because of trying
     # to start the TimerPocket's worker thread multiple times).
     pocket.erase()
 
+    # The JSON file should be empty.
+    pocket.forceSave()
+    assert _load_settings_from_pocket_file(pocket) == {}
+
+    # Pocket is empty after the reset.
+    assert not pocket.has("my_int")
+    assert not pocket.has("my_string")
+    assert not pocket.has("sub_pocket")
+
 
 def test_backup_and_setup(make_pocket):
-    pocket = make_pocket(save_interval=1)
-    print(f'@@ {pocket.getFilename()}')
+    """A NonVolatilePocket can be reset, while preserving/forcing a subset of data"""
+
+    pocket: NonVolatilePocket = make_pocket(save_interval=1)
+
     pocket.setAsInt("my_int", 42)
+    pocket.setAsInt("my_int_preserved", 43)
+    pocket.setAsInt("my_int_overwritten", 2)
     pocket.setAsString("my_string", "FooBar")
+    pocket.setAsString("my_string_preserved", "BamBaz")
+    pocket.setAsString("my_string_overwritten", "BaxBay")
+    pocket.setAsSubPocket("sub_pocket", Pocket({"my_float": 3.14, "foo": "bar"}))
+    pocket.forceSave()
+
+    # We can reset a NonVolatilePocket, like with `erase()`, but...
+    pocket.backupAndSetup(
+        # ... preserving certain bits of data:
+        keys_to_backup=["my_int_preserved", "my_string_preserved"],
+        # ... and setting new bits of data to default values:
+        settings_to_add={"my_int_overwritten": 10, "my_string_overwritten": "Giraffe"}
+    )
+
+    assert _load_settings_from_pocket_file(pocket) == {
+        # Data that was preserved:
+        "my_int_preserved": 43,
+        "my_string_preserved": "BamBaz",
+        # Data that was overwritten:
+        "my_int_overwritten": 10,
+        "my_string_overwritten": "Giraffe",
+        # ...and other data was removed!
+    }
